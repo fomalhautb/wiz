a5ba9755a11c757821a4d6731ef953e0961aee9e
Zai Shi <1179961252@qq.com>
2023-05-10 20:15:12 +0200

    Initial commit

---
+++ source/cli.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+		Usage
+		  $ wiz
+
+		Options
+			--name  Your name
+
+		Examples
+		  $ wiz --name=Jane
+		  Hello, Jane
+	`,
+	{
+		importMeta: import.meta,
+	},
+);
+
+render(<App name={cli.flags.name} />);
